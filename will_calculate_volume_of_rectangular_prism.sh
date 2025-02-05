2cdd667d104d80df47f1f64ee5503d3a8e3b416d
Nirajanconestoga <nbista7839@conestogac.on.ca>
2025-02-05 14:55:03 -0500

    will_calculate_volume_of_rectangular_prism: verifying the test gets pass with the update of the value

--- will_calculate_volume_of_rectangular_prism.sh
+++ will_calculate_volume_of_rectangular_prism.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Calculate the volume of a rectangular prism
 Result=$(./calculate_volumn.sh 1 2 3)
-if [ $Result -eq 4 ]; then
+if [ $Result -eq 6 ]; then
     echo "Test passed"
 else
     echo "Test failed"
